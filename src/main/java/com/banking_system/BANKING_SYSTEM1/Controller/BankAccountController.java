2d430b7d22d7856f59d5928330516045a8d4ca4f
BisenPiyushoo1 <piyushbisen628@gmail.com>
2026-05-17 23:56:32 +0530

    added banking features controller accountcreate

--- src/main/java/com/banking_system/BANKING_SYSTEM1/Controller/BankAccountController.java
+++ src/main/java/com/banking_system/BANKING_SYSTEM1/Controller/BankAccountController.java
@@ -1,4 +1,15 @@
 package com.banking_system.BANKING_SYSTEM1.Controller;
 
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/accountCreate")
+
+@CrossOrigin("*")
 public class BankAccountController {
+
+
 }
