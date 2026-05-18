89c44878ef1942181b971f2512db53d5df198bb2
BisenPiyushoo1 <piyushbisen628@gmail.com>
2026-05-18 23:53:22 +0530

    PublicController

--- src/main/java/com/banking_system/BANKING_SYSTEM1/Controller/PublicController.java
+++ src/main/java/com/banking_system/BANKING_SYSTEM1/Controller/PublicController.java
@@ -1,7 +1,16 @@
 package com.banking_system.BANKING_SYSTEM1.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/public")
 public class PublicController {
+
+    @GetMapping("/about")
+    public String About(){
+        return
+    }
+
 }
