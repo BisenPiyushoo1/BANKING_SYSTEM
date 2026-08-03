dcbcdb6b288a444f8ac2e72440bc2c39a1545eb0
BisenPiyushoo1 <piyushbisen628@gmail.com>
2026-08-03 21:53:27 +0530

    practice

---
+++ src/main/java/com/banking_system/BANKING_SYSTEM1/entity/Transaction.java
@@ -0,0 +1,11 @@
+package com.banking_system.BANKING_SYSTEM1.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="transaction")
+public class Transaction {
+
+
+}
