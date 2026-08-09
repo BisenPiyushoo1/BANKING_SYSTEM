01734ce386b5ae2fba77eef60a9882f910f9463c
BisenPiyushoo1 <piyushbisen628@gmail.com>
2026-08-09 23:41:37 +0530

    db design

--- src/main/java/com/banking_system/BANKING_SYSTEM1/repository/AccountRepository.java
+++ src/main/java/com/banking_system/BANKING_SYSTEM1/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.banking_system.BANKING_SYSTEM1.repository;
 
 import com.banking_system.BANKING_SYSTEM1.entity.Account;
+import org.hibernate.Hibernate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
